0e7674f0ce52f33f2c8685fc333adeb5f48f9c6f
nahida1234 <nahida_036@yahoo.com>
2014-09-28 22:56:37 -0400

    Circle App

---
+++ CircleApp/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Circle
+    {
+        private double radius;
+
+        public double _radius
+        {
+            set { radius = value; }
+            get { return radius; }
+        }
+
+        public double Diameter()
+        {
+            return 2 * _radius;
+        }
+        public double Perimeter()
+         {
+             return 2 * Math.PI * _radius;
+         }
+        public double Area()
+         {
+             return Math.PI * _radius * _radius;
+         }
+    }
+}
---
+++ CircleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle aCircle=new Circle();
+            aCircle._radius = 100;
+
+            
+            Console.WriteLine("Diameter is :" + aCircle.Diameter());
+            Console.WriteLine("Perimeter is :" + aCircle.Perimeter());
+            Console.WriteLine("Area is :" + aCircle.Area());
+
+            Console.ReadKey();
+
+        }
+    }
+}
